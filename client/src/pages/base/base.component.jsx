394fbee68077fb70708c557e8c4f133bd0137999
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-05-09 01:05:32 +0700

    add (client): implement base page

---
+++ client/src/pages/base/base.component.jsx
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types';
+
+import { Tooltip, IconButton, useTheme, makeStyles } from '@material-ui/core';
+import { ArrowBackIosOutlined } from '@material-ui/icons';
+
+import {
+  BasePageStyles,
+  HeaderStyles,
+  TitleStyles,
+  SubtitleStyles
+} from './base.styles';
+
+const useStyles = makeStyles({
+  iconButton: {
+    position: 'fixed',
+    top: 52,
+    left: 85,
+    zIndex: 99
+  }
+});
+
+const BasePage = ({
+  title,
+  subtitle,
+  children,
+  showHeaderButton,
+  tooltipTitle,
+  headerButtonIcon,
+  headerButtonOnClick
+}) => {
+  const theme = useTheme();
+  const classes = useStyles();
+
+  return (
+    <BasePageStyles>
+      <HeaderStyles backgroundColor={theme.palette.background.default}>
+        <TitleStyles>{title}</TitleStyles>
+      </HeaderStyles>
+      <SubtitleStyles color={theme.palette.text.secondary}>
+        {subtitle}
+      </SubtitleStyles>
+
+      {showHeaderButton && (
+        <Tooltip title={tooltipTitle} arrow>
+          <IconButton
+            className={classes.iconButton}
+            onClick={headerButtonOnClick}
+          >
+            {headerButtonIcon}
+          </IconButton>
+        </Tooltip>
+      )}
+
+      {children}
+    </BasePageStyles>
+  );
+};
+
+BasePage.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  showHeaderButton: PropTypes.bool,
+  tooltipTitle: PropTypes.string,
+  headerButtonIcon: PropTypes.element,
+  headerButtonOnClick: PropTypes.func
+};
+
+BasePage.defaultProps = {
+  title: '',
+  subtitle: '',
+  showHeaderButton: false,
+  tooltipTitle: '',
+  headerButtonIcon: <ArrowBackIosOutlined />,
+  headerButtonOnClick: () => {}
+};
+
+export default BasePage;
---
+++ client/src/pages/base/base.styles.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+import themes from '../../themes';
+
+export const BasePageStyles = styled.div`
+  top: -65px;
+  margin-left: 80px;
+  padding: 0 ${themes.lengthMd2} ${themes.lengthMd2};
+`;
+
+export const HeaderStyles = styled.div`
+  position: fixed;
+  top: 0;
+  left: 80px;
+  width: 94%;
+  height: 65px;
+  z-index: 1099;
+  text-align: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+export const TitleStyles = styled.h2``;
+
+export const SubtitleStyles = styled.div`
+  text-align: center;
+  margin-bottom: ${themes.lengthMd3};
+  font-size: 1rem;
+  color: ${({ color }) => color};
+`;
