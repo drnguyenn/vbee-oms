55420d245998737a037c03abb6f7187f09b7fe57
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-05-16 10:46:23 +0700

    feat (client): implement PreferencesModal component

---
+++ client/src/modals/preferences/preferences.component.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  makeStyles
+} from '@material-ui/core';
+
+import { togglePreferencesModal } from '../../redux/modal/modal.actions';
+import { switchTheme } from '../../redux/theme/theme.actions';
+
+import themes from '../../themes';
+
+const useStyles = makeStyles({
+  formLabel: {
+    marginBottom: themes.lengthSm1
+  }
+});
+
+const PreferencesModal = () => {
+  const classes = useStyles();
+
+  const { openPreferencesModal } = useSelector(state => state.modal);
+  const { type } = useSelector(state => state.theme);
+
+  const [value, setValue] = useState(type);
+
+  const dispatch = useDispatch();
+
+  const handleClose = () => dispatch(togglePreferencesModal());
+
+  const handleChange = event => {
+    setValue(event.target.value);
+
+    dispatch(switchTheme(event.target.value));
+  };
+
+  return (
+    <Dialog fullWidth open={openPreferencesModal} onClose={handleClose}>
+      <DialogTitle>Preferences</DialogTitle>
+      <DialogContent>
+        <FormControl>
+          <FormLabel className={classes.formLabel}>THEME</FormLabel>
+          <RadioGroup value={value} onChange={handleChange}>
+            <FormControlLabel
+              value='dark'
+              control={<Radio color='primary' />}
+              label='Dark'
+            />
+            <FormControlLabel
+              value='light'
+              control={<Radio color='primary' />}
+              label='Light'
+            />
+          </RadioGroup>
+        </FormControl>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PreferencesModal;
