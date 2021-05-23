d80ac7756cc25f70cc51e61f6055924b5998ce82
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-05-24 01:14:45 +0700

    feat (client): implement ServiceCard component

---
+++ client/src/components/service-card/service-card.styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleStyles = styled.h3``;
+
+export const TitleCaptionStyles = styled.span`
+  margin-top: -10px;
+`;
+
+export const SubtitleStyles = styled.p`
+  height: 40px;
+`;
+
+export const ServiceMetricsStyles = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+`;
