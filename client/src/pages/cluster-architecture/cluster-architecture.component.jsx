243ea150c67ccda5b9d2c26092967e03144fd9b7
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-06-01 17:58:33 +0700

    feat (client): implement ClusterArchitecturePage

---
+++ client/src/pages/cluster-architecture/cluster-architecture.component.jsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { fetchClusterDiagramStart } from '../../redux/diagram/diagram.actions';
+
+import BasePage from '../base/base.component';
+import Spinner from '../../components/spinner/spinner.component';
+import ClusterArchitectureDiagram from '../../components/cluster-architecture-diagram/cluster-architecture-diagram.component';
+import DiagramToolbar from '../../components/diagram/diagram-toolbar/diagram-toolbar.component';
+import SyncIndicator from '../../components/sync-indicator/sync-indicator.component';
+
+import ROUTE_PATHS from '../../router/route-paths';
+
+const ClusterArchitecturePage = () => {
+  const { id } = useParams();
+
+  const { currentDiagram, isFetchingDiagram, isSynchronizing, error } =
+    useSelector(state => state.diagram);
+
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchClusterDiagramStart(id));
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (error)
+      if (error.message === 'Cluster diagram not found')
+        history.push(ROUTE_PATHS.NOT_FOUND);
+  }, [error, history]);
+
+  const handleHeaderButtonClick = () =>
+    history.push(`${ROUTE_PATHS.CLUSTERS}/${id}`);
+
+  if (isFetchingDiagram) return <Spinner />;
+
+  if (currentDiagram)
+    return (
+      <BasePage
+        title={
+          currentDiagram.cluster &&
+          `${currentDiagram.cluster.name} Architecture`
+        }
+        showHeaderButton
+        tooltipTitle={
+          currentDiagram.cluster &&
+          `Back to "${currentDiagram.cluster.name}" Cluster Page`
+        }
+        headerButtonOnClick={handleHeaderButtonClick}
+      >
+        <ClusterArchitectureDiagram />
+        <DiagramToolbar />
+        <SyncIndicator spin={isSynchronizing} />
+      </BasePage>
+    );
+
+  return null;
+};
+
+export default ClusterArchitecturePage;
