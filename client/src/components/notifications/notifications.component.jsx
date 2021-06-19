d15e2d10d612b46ba2e53b10c9fdde5952c3ad79
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-06-19 16:55:39 +0700

    update (client): notify network connection status

--- client/src/components/notifications/notifications.component.jsx
+++ client/src/components/notifications/notifications.component.jsx
@@ -1,17 +1,56 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useSnackbar } from 'notistack';
 
+import { Typography } from '@material-ui/core';
+
+const ConnectionErrorContent = () => (
+  <Typography variant='body2'>
+    You are currently offline.
+    <br />
+    Some features may not work properly
+  </Typography>
+);
+
 const Notifications = () => {
-  const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   const notification = useSelector(state => state.notification);
 
-  useEffect(() => {
-    const { type, content } = notification;
+  const [notiKey, setNotiKey] = useState();
 
-    if (content) enqueueSnackbar(content, { variant: type });
+  useEffect(() => {
+    const { content, options } = notification;
+
+    if (content) enqueueSnackbar(content, options);
   }, [notification, enqueueSnackbar]);
+
+  useEffect(() => {
+    navigator.connection.onchange = () => {
+      if (navigator.onLine) {
+        if (notiKey) {
+          closeSnackbar(notiKey);
+          setNotiKey(null);
+        }
+
+        setNotiKey(
+          enqueueSnackbar('Your connection was restored', { variant: 'info' })
+        );
+      } else {
+        if (notiKey) {
+          closeSnackbar(notiKey);
+          setNotiKey(null);
+        }
+
+        setNotiKey(
+          enqueueSnackbar(<ConnectionErrorContent />, {
+            variant: 'error',
+            persist: true
+          })
+        );
+      }
+    };
+  }, [enqueueSnackbar, closeSnackbar, notiKey]);
 
   return null;
 };
