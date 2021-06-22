9a8075a84f34e1424055a6f34ed8bb15dd2eca94
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-06-22 15:55:27 +0700

    feat (client): implement ServerDomainRemoveConfirmation modal

---
+++ client/src/modals/server-domain-remove-confirmation/server-domain-remove-confirmation.component.jsx
@@ -0,0 +1,72 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button
+} from '@material-ui/core';
+
+import { toggleServerDomainRemoveConfirmationModal } from '../../redux/modal/modal.actions';
+import { removeServerDomainStart } from '../../redux/server/server.actions';
+
+const ServerDomainRemoveConfirmationModal = () => {
+  const { openServerDomainRemoveConfirmationModal } = useSelector(
+    state => state.modal
+  );
+
+  const { currentDomain, isRemovingDomains } = useSelector(
+    state => state.server
+  );
+
+  const dispatch = useDispatch();
+
+  const handleClose = () =>
+    dispatch(toggleServerDomainRemoveConfirmationModal());
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (currentDomain.id) dispatch(removeServerDomainStart(currentDomain.id));
+  };
+
+  if (currentDomain)
+    return (
+      <Dialog
+        open={openServerDomainRemoveConfirmationModal}
+        onClose={handleClose}
+        fullWidth
+      >
+        <DialogTitle>Remove domain</DialogTitle>
+        <DialogContent>
+          <Typography gutterBottom>
+            Remove domain <b>{currentDomain.value}</b> ?
+          </Typography>
+        </DialogContent>
+
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            variant='outlined'
+            disabled={isRemovingDomains}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleSubmit}
+            variant='contained'
+            color='secondary'
+            disabled={isRemovingDomains}
+          >
+            Remove domain
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+
+  return null;
+};
+
+export default ServerDomainRemoveConfirmationModal;
