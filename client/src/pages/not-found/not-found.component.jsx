39a654ea3bf3193530c32ead4494851a22380e8d
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2021-06-27 19:06:21 +0700

    update (client): update 404 error page's image

--- client/src/pages/not-found/not-found.component.jsx
+++ client/src/pages/not-found/not-found.component.jsx
@@ -2,9 +2,9 @@ import { useHistory } from 'react-router-dom';
 
 import { Fab, Typography, makeStyles } from '@material-ui/core';
 
-import BasePage from '../base/base.component';
+import BasePage from 'pages/base/base.component';
 
-import NotFoundPageImage from '../../assets/images/not-found-page.svg';
+import NotFoundPageImage from 'assets/images/not-found-page.svg';
 
 import { NotFoundImageStyles } from './not-found.styles';
 
@@ -23,10 +23,10 @@ const NotFoundPage = () => {
     <BasePage textAlign='center'>
       <NotFoundImageStyles src={NotFoundPageImage} alt='404 Page Image' />
       <Typography variant='h4' component='h1' gutterBottom>
-        Oops, something &apos;s missing...
+        Oops, something is missing...
       </Typography>
       <Typography className={classes.content}>
-        It seems like we donut find what you searched.
+        It seems like we do not find what you want.
         <br />
         The page you were looking for does not exist, is unavailable or was
         loading incorrectly.
--- client/src/pages/not-found/not-found.styles.jsx
+++ client/src/pages/not-found/not-found.styles.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const NotFoundImageStyles = styled.img`
   width: 35%;
+  margin: -50px 0;
 
   @media screen and (max-width: 650px) {
     width: 80%;
